ce8dcee4b085cf2fa3cf3f1a5cc54b81515ca28a
LE VAN THANH <zero.lethanh@gmail.com>
2022-12-16 12:27:33 +0700

    update example

--- example/main.go
+++ example/main.go
@@ -16,4 +16,22 @@ func main() {
 		admin_client.Storage,
 		admin_client.DefaultBucket,
 	)
+
+	admin_client.EnvCollPrefix = "GO_COL_"
+	fmt.Println(admin_client.CollPathFromEnv("GO_COL_USERS"))
+
+	admin_client.CollRef("GO_COL_USERS" /* or "users"*/)
+	admin_client.CollGetAllDocs("users")
+	admin_client.CollGetAllDocsData("users")
+
+	admin_client.DocRef("users", "123")
+	admin_client.GetDoc("users", "123")
+	admin_client.GetDocData("users", "123")
+
+	type User struct {
+		Name string
+	}
+	var adam = new(User)
+	admin_client.GetDocDataTo("users", "123", adam)
+
 }
